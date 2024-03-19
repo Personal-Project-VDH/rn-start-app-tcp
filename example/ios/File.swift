cfbfd8ba263431b23d95dc6bd20e7ad93eab3f9e
Vũ Đức Hoàng <vdhoang17112001@gmail.com>
2024-03-19 14:34:16 +0700

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  RnStartAppTcpExample
+//
+
+import Foundation
---
+++ ios/RnStartAppTcp.swift
@@ -0,0 +1,8 @@
+@objc(RnStartAppTcp)
+class RnStartAppTcp: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
